5b0b1dcf1e084bc27fe949ae02dd4b05038398fc
Andre I <aisaac2020@bklawtech.com>
2017-02-16 21:15:43 +0000

    house

---
+++ script.js
@@ -0,0 +1,20 @@
+TinyTurtle.apply(window);
+
+/*global TinyTurtle*/
+
+forward(60);
+right(90);
+forward(50);
+right(90);
+forward(60);
+right(90);
+forward(50);
+right(90);
+forward(60);
+right(45);
+forward(60);
+right(90);
+forward(30);
+right(45);
+forward(20);
+stamp();
